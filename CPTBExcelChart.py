8a68241005c12b11fd3761e5ee5b6cf76414ec1e
mgg-eagle <marc_graham@eagle.co.nz>
2017-03-13 14:51:20 +1300

    uncomment upload to arcgis online

--- CPTBExcelChart.py
+++ CPTBExcelChart.py
@@ -44,7 +44,7 @@ class CPTBExcelChart(Base):
 
     def do_reporting(self):
         self._create_report_for_service()
-        self._upload_to_arcgis_online() #make a change
+        self._upload_to_arcgis_online()
 
     def _create_report_for_service(self):
         try:
