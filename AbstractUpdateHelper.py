c69e4e439e7e6b5fe3fcffd1755bac84b299179c
cleanlines <fraser.hand@gmail.com>
2017-06-07 12:19:57 +1200

    first cut

--- AbstractUpdateHelper.py
+++ AbstractUpdateHelper.py
@@ -17,11 +17,9 @@ class AbstractUpdateHelper(object):
     def get_related_records(self, recs, relationship_id):
         raise NotImplementedError()
 
-
     @abstractmethod
     def update_features_asset_info(self):
         raise NotImplementedError()
-
 
     @abstractmethod
     def update_areas_for_features(self):
